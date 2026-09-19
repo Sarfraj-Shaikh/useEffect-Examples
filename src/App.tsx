4215d1750e6024aae3a93fa49548efb450126db0
Sarfraj Shaikh <work.shaikhmosarfraj@gmail.com>
2026-09-19 15:08:58 +0530

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
 import Example1 from "./Components/Ex1";
 import Example2 from "./Components/Ex2";
 import Example3 from "./Components/Ex3";
@@ -15,7 +16,8 @@ function App() {
   return (
 
     <Routes>
-      <Route path="/" element={<Example1 />} />
+      
+      <Route path="/" element={<Home />} />
 
       <Route path="/1" element={<Example1 />} />
       <Route path="/2" element={<Example2 />} />
