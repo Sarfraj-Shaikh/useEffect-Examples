b028122fee595c60f7b2e727967037868b22339a
Sarfraj Shaikh <work.shaikhmosarfraj@gmail.com>
2026-09-19 14:54:42 +0530

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Example1 from "./Components/Ex1";
 import Example2 from "./Components/Ex2";
 import Example3 from "./Components/Ex3";
 import Example4 from "./Components/Ex4";
+import Example5 from "./Components/Ex5";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Route path="/2" element={<Example2 />} />
       <Route path="/3" element={<Example3 />} />
       <Route path="/4" element={<Example4 />} />
+      <Route path="/5" element={<Example5 />} />
 
     </Routes>
 
